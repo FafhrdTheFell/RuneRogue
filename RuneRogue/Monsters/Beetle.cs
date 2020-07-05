1f81ca32202f7eab4c78401780469ba919429eeb
FafhrdTheFell <fafhrdthefell@gmail.com>
2020-07-05 10:41:31 -0400

    Added beetles and fixed formatting

---
+++ RuneRogue/Monsters/Beetle.cs
@@ -0,0 +1,27 @@
+﻿using RogueSharp.DiceNotation;
+using RuneRogue.Core;
+
+namespace RuneRogue.Monsters
+{
+   public class Beetle : Monster
+   {
+      public static Beetle Create( int level )
+      {
+         int health = Dice.Roll( "2D3" );
+         return new Beetle {
+            Attack = Dice.Roll( "1D3" ) + level / 3,
+            AttackChance = Dice.Roll( "25D3" ),
+            Awareness = 10,
+            Color = Colors.BeetleColor,
+            Defense = Dice.Roll( "2D2" ) + level / 3,
+            DefenseChance = Dice.Roll( "10D4" ),
+            Gold = 0,
+            Health = health,
+            MaxHealth = health,
+            Name = "Beetle",
+            Speed = 7,
+            Symbol = 'b'
+         };
+      }
+   }
+}
